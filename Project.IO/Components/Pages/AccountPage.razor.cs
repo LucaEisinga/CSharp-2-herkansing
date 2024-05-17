d2b72702ec6e5cb3f96c64bc31675fb343f1c60b
robin <robenjay@hotmail.nl>
2024-05-17 12:16:00 +0200

    added project page, with modal but doesn't work yet

---
+++ Project.IO/Components/Pages/AccountPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using BootstrapBlazor.Components;
+
+namespace Project.IO.Components.Pages
+{
+    public partial class AccountPage
+    {
+
+        /*private Modal modal = default!;
+
+        private async Task OnShowModalClick()
+        {
+            await modal.Show();
+        }
+
+        private async Task OnHideModalClick()
+        {
+            await modal.Close();
+        }
+*/
+    }
+}
