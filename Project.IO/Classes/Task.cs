8205097b4e0f744597d6544ade2595f9c6683467
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2024-05-28 13:16:47 +0200

    class diagram classes added

---
+++ Project.IO/Classes/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.IO.Classes
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public List<Member> members { get; set; }
+        public DatePicker deadline { get; set; }
+        public string description { get; set; }
+        public int difficulty { get; set; }
+
+        public Task(string title, string description, int difficulty)
+        {
+            this.title = title;
+            this.description = description;
+            this.difficulty = difficulty;
+            this.members = new List<Member>();
+            deadline = new DatePicker();
+        }
+        
+        public void addMemberToTask(Member member)
+        {
+            members.Add(member);
+        }
+    }
+}
