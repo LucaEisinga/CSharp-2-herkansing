a71f7ef1bdde0ab69c276ee3f2b50e7225603e94
robin <robenjay@hotmail.nl>
2024-05-30 11:18:26 +0200

    added the front-end of the add task page, like a modal

---
+++ Project.IO/Components/Pages/TaskPage.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorBootstrap;
+using System.Reflection;
+
+namespace Project.IO.Components.Pages
+{
+    public partial class TaskPage
+    {
+
+        private Modal taskModal = default!;
+
+        private async Task ShowTaskModal()
+        {
+            await taskModal.ShowAsync();
+        }
+
+    }
+}
