9ef1b3418f3e1de0b9a8d283c7f1a53d1dc1b17d
robin <robenjay@hotmail.nl>
2024-06-19 13:39:14 +0200

    file can be uploaded but there are a couple of problems, while doing so. If i upload a second time in the same session, it starts giving problems about permission

---
+++ Project.IO/Components/Pages/DocumentOverviewPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Project.IO.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.IO.Components.Pages
+{
+    partial class DocumentOverviewPage
+    {
+
+        private FileUtil fileUtil = new FileUtil();
+
+        private async Task FileUploading(InputFileChangeEventArgs e)
+        {
+            await fileUtil.HandleFileSelected(e);
+        }
+
+    }
+}
--- Project.IO/Utilities/FileUtil.cs
+++ Project.IO/Utilities/FileUtil.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
 
 namespace Project.IO.Utilities
 {
     internal class FileUtil
     {
+        public async Task HandleFileSelected(InputFileChangeEventArgs e)
+        {
+            var uploadDirectory = Path.Combine(AppContext.BaseDirectory, "ProjectFiles");
+
+            // Ensure directory exists
+            if (!Directory.Exists(uploadDirectory))
+            {
+                Directory.CreateDirectory(uploadDirectory);
+            }
+
+            foreach (var file in e.GetMultipleFiles())
+            {
+                var path = Path.Combine(uploadDirectory, file.Name);
+
+                // Save the file
+                using var stream = file.OpenReadStream();
+                using var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+                await stream.CopyToAsync(fileStream);
+
+                Debug.WriteLine($"File saved to {path}");
+            }
+        }
     }
 }
